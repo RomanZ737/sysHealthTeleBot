f832858865ea7ac8f22650f362d5b0118b44b4b9
Roman <pomanz@mail.ru>
2022-12-18 23:38:10 +0300

    Add Comman faults

--- main.py
+++ main.py
@@ -1,11 +1,35 @@
 import telebot
+from telebot import types
 from decouple import config
 from pymysql.cursors import DictCursor  # Возвразает курсор в виде словаря из базы данных
 import pymysql.connections
 import pymysql  # модуль работы с MySQL
 
 token = config("sys_health_bot_taken", default='')
+chat_id = config("sys_health_bot_chat_id", default='')
 bot = telebot.TeleBot(token)
+
+keyButton = types.InlineKeyboardMarkup()
+btn1 = types.InlineKeyboardButton(text='FAULTS', callback_data='/faults')
+#btn2 = types.InlineKeyboardButton(text='Temp')
+
+keyButton.add(btn1)
+
+
+# def start():
+#     bot.send_message(chat_id=chat_id, text='Hello', reply_markup=markup)
+#
+# start()
+
+@bot.message_handler(content_types=['text'])
+def text_test(message):
+    #bot.send_message(message.chat.id, "Test button", reply_markup=keyButton)
+    if message.text == 'FAULTS':
+        bot.send_message(message.chat.id, "Here is some info")
+
+@bot.message_handler(commands=['start'])
+def handle_start(message):
+    bot.reply_to(message, 'Hello', reply_markup=keyButton)
 
 
 @bot.message_handler(commands=['faults'])  # формирует список активных ошибок и базы данных
@@ -30,12 +54,12 @@ def fault_list(message):
 # @bot.message_handler(content_types=['text'])
 # def get_text_messages(message):
 #     bot.send_message(message.from_user.id, message)
-    # if message.text == "Привет":
-    #     bot.send_message(message.from_user.id, "Привет, чем я могу тебе помочь?")
-    # elif message.text == "/help":
-    #     bot.send_message(message.from_user.id, "Напиши привет")
-    # else:
-    #     bot.send_message(message.from_user.id, "Я тебя не понимаю. Напиши /help.")
+# if message.text == "Привет":
+#     bot.send_message(message.from_user.id, "Привет, чем я могу тебе помочь?")
+# elif message.text == "/help":
+#     bot.send_message(message.from_user.id, "Напиши привет")
+# else:
+#     bot.send_message(message.from_user.id, "Я тебя не понимаю. Напиши /help.")
 
 
 bot.polling(none_stop=True, interval=0)
