088ca0438ccba72ea3235b2f1f3e0ff95d17193d
Roman <pomanz@mail.ru>
2022-12-19 14:59:35 +0300

    Emoji

--- main.py
+++ main.py
@@ -9,46 +9,45 @@ token = config("sys_health_bot_taken", default='')
 chat_id = config("sys_health_bot_chat_id", default='')
 bot = telebot.TeleBot(token)
 
-keyButton = types.InlineKeyboardMarkup()
-btn1 = types.InlineKeyboardButton(text='FAULTS', callback_data='/faults')
-#btn2 = types.InlineKeyboardButton(text='Temp')
+keyButton = types.ReplyKeyboardMarkup(row_width=3, resize_keyboard=True)
+faults_button = types.KeyboardButton(text='Faults')
+status_button = types.KeyboardButton(text='Server Status')
 
-keyButton.add(btn1)
+keyButton.add(faults_button, status_button)
 
 
-# def start():
-#     bot.send_message(chat_id=chat_id, text='Hello', reply_markup=markup)
-#
-# start()
+def start():
+     bot.send_message(chat_id=chat_id, text='Hello', reply_markup=keyButton)
+
+start()
 
 @bot.message_handler(content_types=['text'])
 def text_test(message):
     #bot.send_message(message.chat.id, "Test button", reply_markup=keyButton)
-    if message.text == 'FAULTS':
-        bot.send_message(message.chat.id, "Here is some info")
+    if message.text == 'Faults':
+        """MySQL Connection Block"""
+        connection = pymysql.connect(  # Коннектимся к базе MySQL
+            host=config('ip_mysql', default=''),
+            user=config("SQL_usrID", default=''),
+            password=config('SQL_password', default=''),
+            db=config('SQL_DB', default=''),
+            charset='utf8mb4',
+            cursorclass=DictCursor  # Курсор будет возвращать значения в виде словарей
+        )
+        cur = connection.cursor()  # Создаём курсор SQL
+        request = f"SELECT * FROM other_faults"
+        cur.execute(request)
+        for j in cur.fetchall():
+            text = f'❌ FAULT:\nServer: <b>{j["name"]}</b>, fault: <b>{j["fault_name"]}</b>'
+            bot.send_message(message.from_user.id, text, parse_mode='html')
+            #bot.send_message(message.from_user.id, (f'Server: {j["name"]}, fault: {j["fault_name"]}'))
+        connection.commit()
+#✅ FIXED\n
 
-@bot.message_handler(commands=['start'])
-def handle_start(message):
-    bot.reply_to(message, 'Hello', reply_markup=keyButton)
 
+# @bot.message_handler(commands=['faults'])  # формирует список активных ошибок и базы данных
+# def fault_list(message):
 
-@bot.message_handler(commands=['faults'])  # формирует список активных ошибок и базы данных
-def fault_list(message):
-    """MySQL Connection Block"""
-    connection = pymysql.connect(  # Коннектимся к базе MySQL
-        host=config('ip_mysql', default=''),
-        user=config("SQL_usrID", default=''),
-        password=config('SQL_password', default=''),
-        db=config('SQL_DB', default=''),
-        charset='utf8mb4',
-        cursorclass=DictCursor  # Курсор будет возвращать значения в виде словарей
-    )
-    cur = connection.cursor()  # Создаём курсор SQL
-    request = f"SELECT * FROM other_faults"
-    cur.execute(request)
-    for j in cur.fetchall():
-        bot.send_message(message.from_user.id, (f'Server: {j["name"]}, fault: {j["fault_name"]}'))
-    connection.commit()
 
 
 # @bot.message_handler(content_types=['text'])
